981d33cfa230fa20b897cc2f30660cd37348a388
SgtMarmite <d.novotny31415@gmail.com>
2022-11-16 16:33:15 +0100

    init version

--- src/interactive_table.py
+++ src/interactive_table.py
@@ -1,36 +1,48 @@
 import streamlit as st
-from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
+from st_aggrid import GridOptionsBuilder
 import pandas as pd
-from pathlib import Path
+import math
+
+
+def convert_size(size_bytes):
+    if size_bytes == 0:
+        return "0B"
+    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+    i = int(math.floor(math.log(size_bytes, 1024)))
+    p = math.pow(1024, i)
+    s = round(size_bytes / p, 2)
+    return "%s %s" % (s, size_name[i])
 
 
 def interactive_table():
-    query_df = pd.read_csv(st.session_state['uploaded_file'])
-    st.header(f"📊 Explore {Path(st.session_state['uploaded_file']).name}")
+    query_df = pd.read_csv(st.session_state['extracted_file'])
+    query_df['time'] = pd.to_datetime(query_df['time'], unit='s')
 
-    def aggrid_interactive_table(df: pd.DataFrame):
-        """Creates an st-aggrid interactive table based on a dataframe.
+    stats = ["cpu_percent", "ram_percent", "interval_net_in", "interval_net_out", "total_net_in", "total_net_out"]
+
+    def aggrid_interactive_graph(df: pd.DataFrame, field):
+        """Creates a graph based on a dataframe.
         Args:
-            df (pd.DataFrame]): Source dataframe
+            df (pd.DataFrame): Source dataframe
+            field (string): name of the stat to print
         Returns:
             dict: The selected row
         """
+        st.header(f"📊 Stat: {field}")
         options = GridOptionsBuilder.from_dataframe(
             df, enableRowGroup=True, enableValue=True, enablePivot=True
         )
 
         options.configure_side_bar()
         options.configure_selection("single")
-        selection = AgGrid(
-            df,
-            enable_enterprise_modules=True,
-            gridOptions=options.build(),
-            theme="streamlit",
-            height=800,
-            update_mode=GridUpdateMode.MODEL_CHANGED,
-            allow_unsafe_jscode=True,
-        )
 
-        return selection  # return the selected row
+        return st.line_chart(data=df, x="time", y=field)
 
-    selection = aggrid_interactive_table(df=query_df)  # create the table
+    for stat in stats:
+        aggrid_interactive_graph(df=query_df, field=stat)  # create the table
+
+    formated_columns = ["ram_used", "interval_net_in", "interval_net_out", "total_net_in", "total_net_out"]
+    for col in formated_columns:
+        query_df[col] = query_df[col].apply(convert_size)
+
+    st.write(query_df)
